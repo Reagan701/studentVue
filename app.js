b3a299536247b02bedd963bf4a7f353a5322bb29
Reagan701 <kingreggie702@gmail.com>
2022-07-04 12:23:47 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const nameApp = Vue.createApp({
+    data: function(){
+        return{
+            name: 'Reagan',
+            surname: 'Carolussen'
+        }
+    }
+})
+
+nameApp.mount('#App');
